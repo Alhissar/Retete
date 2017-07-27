81d3dc64fd0cb71f062b971078217a86c31d1938
Alhissar <30404610+Alhissar@users.noreply.github.com>
2017-07-27 20:56:56 +0200

    Update header.php
    Ajout de la balise meta pour la google font et autres trucs

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://fonts.googleapis.com/css?family=Exo|Titillium+Web" rel="stylesheet">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
