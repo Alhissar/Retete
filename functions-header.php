1e78a3155d8ac4b255826cfb781edeea0aafaa26
Alhissar <30404610+Alhissar@users.noreply.github.com>
2017-07-31 21:08:43 +0200

    ajout de la div header
    code html dans le header

--- functions-header.php
+++ functions-header.php
@@ -13,7 +13,21 @@
 		<p class="site-description"><?php echo $seos_photography_description; /* WPCS: xss ok. */ ?></p>
 	<?php endif; ?>
 
-	<div class="sous_titre">retete</div>
+	<div class="sous_titre">
+		<div class="potar-g">
+			<img class="volume" src="img/volume.png" alt="" width="150px" height="150px">
+			<img class="curseurs" src="img/curseurs-g.png" alt="" width="195px" height="165px">
+		</div>
+		<div class="sous-titre-centre">
+			<p>Studio 7 met sa singularité à votre service et dynamise votre univers musical pour vous différencier, dans le respect de l’ADN de la marque.</p>
+			<img src="img/vumetre.png" alt="" width="642px" height="116px">
+			<p>Votre projet nécessite une VOIX OFF&nbsp;?<br>Pub, radio, TV, institutionnel, voice over, audiotel, livres-audios, doublage, vidéos, narration, documentaire, bande annonce, habillage vocal…</p>
+		</div>
+		<div class="potar-d">
+			<img class="volume" src="img/volume.png" alt="" width="150px" height="150px">
+			<img class="curseurs" src="img/curseurs-d.png" alt="" width="195px" height="165px">
+		</div>
+	</div>
 
 	<?php if(!get_theme_mod('hide_buttn_1')):
 		if(get_theme_mod('seos_photography_button_text1')) : ?>
