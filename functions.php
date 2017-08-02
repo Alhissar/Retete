180022c48b0f1641b05cd7d6418796748e87087b
Alhissar <30404610+Alhissar@users.noreply.github.com>
2017-08-02 11:32:52 +0200

    racine enfant

--- functions.php
+++ functions.php
@@ -5,3 +5,8 @@ add_action( 'wp_enqueue_scripts', 'wpchild_enqueue_styles' );
 function wpchild_enqueue_styles(){
   wp_enqueue_style( 'wpm-seos-photography-style', get_template_directory_uri() . '/style.css' );
 }
+
+function comogene_excerpt_length($length) {
+     return 10;
+}
+add_filter('excerpt_length', 'comogene_excerpt_length');
