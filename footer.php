c97ec179073f24d4124817b542d9b5768f6b8905
Alhissar <30404610+Alhissar@users.noreply.github.com>
2017-08-07 16:35:25 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+
+		<div class="site-info">
+
+			&copy; <?php bloginfo('name'); ?> <br>
+			89560 Fouronnes /
+			Tél : 06 32 55 27 99
+
+		</div><!-- .site-info -->
+
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
